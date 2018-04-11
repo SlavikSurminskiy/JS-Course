7660bd3180e3d5737391ac0048655539c012d8c5
Slavik <slaviksyrminskiy@gmail.com>
2018-04-11 22:03:43 +0300

    Виконані завдання 11 уроку

--- lesson_11/js/script.js
+++ lesson_11/js/script.js
@@ -171,6 +171,52 @@ window.addEventListener('DOMContentLoaded', function() {
  						}
  				});
 
+let form2 = document.getElementById('form'),
+				email = form2.getElementsByTagName('input')[0],
+				phone = form2.getElementsByTagName('input')[1];
+
+				form2.addEventListener('submit', function(event) {
+						event.preventDefault();
+						form.appendChild(statusMessage);
+
+						//AJAX
+						let request = new XMLHttpRequest();
+						request.open("POST", 'server.php');
+
+						request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+						let formData = new FormData(form2);
+
+						request.send(formData);
+
+						request.onreadystatechange = function() {
+
+							if(request.readyState < 4)
+							{
+								statusMessage.innerHTML = message.loading;
+							}
+							else if (request.readyState === 4)
+							{
+								if( request.status == 200 && request.status < 300)
+								{
+									statusMessage.innerHTML = message.success;
+									// додаємо контент на сторінку
+								}
+								else 
+								{
+									statusMessage.innerHTML = message.failure;
+								}
+							}
+
+						}
+						for(let i = 0; i < input.length; i++) {
+							input[i].value = '';
+							//очищуємо поля вводу
+						}
+				});
+
+
+
 
 });
 
